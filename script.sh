e35dab50dd6671ab5d24892fa3696d60f00fccab
asrithapenugonda <asrithapenugonda16121@gmail.com>
2025-02-03 21:02:43 +0000

    aws_tracker project

--- script.sh
+++ script.sh
@@ -3,17 +3,19 @@
 #### and redirecting to a file
 
 ##Listing S3 buckets
-echo -e "\e[32m  list of s3\e[0m"
-aws s3 ls
+
+touch resouceTracker
+echo -e "\e[32m  list of s3\e[0m"     >>resouceTracker
+aws s3 ls   >>resouceTracker
 
 ##listing ec2
-echo -e "\e[32m  list of ec2 instances\e[0m"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+echo -e "\e[32m  list of ec2 instances\e[0m"  >>resouceTracker
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'  >>resouceTracker
 
 #listing aws lamba functions
-echo -e "\e[32m list of lambda functions\e[0m"
-aws lambda list-functions | jq '.Functions[]'
+echo -e "\e[32m list of lambda functions\e[0m"    >>resouceTracker
+aws lambda list-functions | jq '.Functions[]'      >>resouceTracker
 
 #listing iam users
-echo -e "\e[32m list of iam users\e[0m"
-aws iam list-users | jq '.Users[].UserName'
+echo -e "\e[32m list of iam users\e[0m"    >>resouceTracker
+aws iam list-users | jq '.Users[].UserName'    >>resouceTracker
