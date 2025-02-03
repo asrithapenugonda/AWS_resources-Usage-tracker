e797b798a3c7f1271007ce96ca59840c12a19956
asrithapenugonda <asrithapenugonda16121@gmail.com>
2025-02-03 23:31:26 +0000

    aws_tracker project

--- crontab.sh
+++ crontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #script_location=$(pwd)
-CRON_JOB="25 23 * * * /home/ubuntu/AWS_resources-Usage-tracker/script.sh"
+CRON_JOB="31 23 * * * /home/ubuntu/AWS_resources-Usage-tracker/script.sh"
 
 (crontab -l 2>/dev/null; echo ${CRON_JOB}) | crontab -
