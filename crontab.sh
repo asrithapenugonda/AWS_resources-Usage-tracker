5e6a04ebcd388345be827d84dd8bd05bc3d57dc8
asrithapenugonda <asrithapenugonda16121@gmail.com>
2025-02-03 23:40:27 +0000

    aws_tracker project

--- crontab.sh
+++ crontab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#script_location=$(pwd)
-CRON_JOB="* * * * */home/ubuntu/AWS_resources-Usage-tracker/script.sh"
 
-(crontab -l 2>/dev/null; echo ${CRON_JOB}) | crontab -
+# Define the cron job to run every minute
+CRON_JOB="* * * * * /home/ubuntu/AWS_resources-Usage-tracker/script.sh"
+
+# Add the cron job to the crontab
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
