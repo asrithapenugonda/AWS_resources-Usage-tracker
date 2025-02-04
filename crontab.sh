e96965649079c0c28739d2448bc4757f39113b61
asrithapenugonda <asrithapenugonda16121@gmail.com>
2025-02-04 04:04:07 +0000

    aws_tracker project

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the cron job to run every minute
-CRON_JOB="* * * * * /home/ubuntu/AWS_resources-Usage-tracker/script.sh"
+CRON_JOB="* * * * * /home/ubuntu/projects/AWS_resourceTracker.sh"
 
 # Add the cron job to the crontab
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
